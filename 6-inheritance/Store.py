09480580db0492331b66e6b44be1cbc30807c43b
Kender <98911087+kender111@users.noreply.github.com>
2022-04-25 20:36:39 +0300

    Add files via upload
    Add files via upload
    
    Add files via upload
    
    Add files via upload

--- 6-inheritance/Store.py
+++ 6-inheritance/Store.py
@@ -7,7 +7,7 @@ class Product:
 class ProductStore(Product):
     def __init__(self):
         self.storage = {}
-
+'''
     def add(self, Product, anmount):
         self.product = {'TYPE' : Product.ti, 
                         'NAME' : Product.name, 
@@ -27,25 +27,7 @@ class ProductStore(Product):
                     self.product['ANMOUNT'] += self.anmount
         else:
             self.storage[Product] = self.product
-
-    def add(self, Product, anmount):
-       print(self.product, end = '\n\n\n')
-       for i in self.storage.keys():
-            temp_v = 0
-            if i == id(Product): 
-                for ind in self.product.keys():
-                    self.product['ANMOUNT'] += self.anmount
-                    #print(ind, self.product)
-
-                for ind in self.product.values():
-                    self.product['ANMOUNT'] += self.anmount
-        else:
-             self.product = {'TYPE' : Product.ti, 
-                        'NAME' : Product.name, 
-                        'PRICE' : Product.price,
                         'ANMOUNT' : anmount}
->>>>>>> main
-
 
     def set_discount(identifier, percent, identifier_type='name'):
         pass
